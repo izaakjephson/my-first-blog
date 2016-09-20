5973c980392cf3a6614342399f17ea1122ba6d6d
izaak <izaakjephson@outlook.com>
2016-09-20 19:48:01 +0100

    My app, first commit

---
+++ .gitignore.py
@@ -0,0 +1,6 @@
+*.pyc
+__pycache__
+web1
+db.sqlite3
+/static
+.DS_Store
---
+++ web1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/izaakjephson/Documents/Python/website/web1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
